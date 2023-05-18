2aa9f5adf33b2048c265ff8ed7c77feb4bebdfbf
ephemeral98 <3502969036@qq.com>
2023-05-18 17:39:17 +0800

    添加倒计时组件

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,13 +8,71 @@ export default defineConfig({
       {
         text: 'Guide',
         items: [
-          { text: '关闭图标(BpClose)', link: '/BpClose' },
-          { text: '表单组件(BpForm)', link: '/BpForm' },
-          { text: '选项卡组件(BpTab)', link: '/BpTab' },
-          { text: '全局转圈圈(GlobalLoading)', link: '/GlobalLoading' },
-          { text: '指针光标组件(MousePoint)', link: '/MousePoint' },
-          { text: '开屏动画(Welcome)', link: '/Welcome' },
-          { text: 'Swiper(BpSwiper)', link: '/BpSwiper' },
+          { text: 'BpButton', link: '/BpButton/index' },
+          { text: '关闭图标(BpClose)', link: '/BpClose/index' },
+          {
+            text: '表单组件(BpForm)', link: '/BpForm/index',
+            items: [
+              {
+                text: '表单组件(源码)', link: '/BpForm/source'
+              }
+            ],
+            collapsed: true,
+          },
+          {
+            text: '选项卡组件(BpTab)', link: '/BpTab/index',
+            items: [
+              {
+                text: '横向选项卡组件(源码)', link: '/BpTab/source',
+              },
+              {
+                text: '纵向选项卡组件(源码)', link: '/BpTab/colSource',
+              },
+            ],
+            collapsed: true,
+          },
+          {
+            text: '全局转圈圈(GlobalLoading)', link: '/GlobalLoading/index',
+            items: [
+              { text: '全局转圈圈(源码)', link: '/GlobalLoading/source' },
+            ],
+            collapsed: true,
+          },
+          {
+            text: '指针光标组件(MousePoint)', link: '/MousePoint/index',
+            items: [
+              { text: '指针光标组件(源码)', link: '/MousePoint/source' },
+            ],
+            collapsed: true,
+          },
+          {
+            text: '开屏动画(Welcome)', link: '/Welcome/index',
+            items: [
+              { text: '开屏动画(源码)', link: '/Welcome/source', }
+            ],
+            collapsed: true,
+          },
+          {
+            text: 'Swiper(BpSwiper)', link: '/BpSwiper/index',
+            items: [
+              {
+                text: 'BpSwiper(源码)', link: '/BpSwiper/source'
+              }
+            ],
+            collapsed: true,
+          },
+          {
+            text: '倒计时(Countdown)', link: '/Countdown/index',
+            items: [
+              {
+                text: '离开始与离结束(组件)(源码)', link: '/Countdown/source'
+              },
+              {
+                text: 'useCountdown(源码)', link: '/Countdown/hookSource'
+              }
+            ],
+            collapsed: true,
+          },
         ],
         collapsible: true,
         collapsed: false,
--- docs/Welcome/source.md
+++ docs/Welcome/source.md
@@ -1,28 +1,4 @@
-## 开屏动画组件
-
-### 使用方法
-一般在首页直接引入即可
-
-控制显示隐藏的状态最好全局共享
-
-```ts
-// appStore.ts
-const useAppStore = defineStore('app', {
-  state: () => ({
-    welcoming: false, // 开场动画视频
-  }),
-})
-```
-
-```jsx
-const appStore = useAppStore();
-
-const { welcoming } = storeToRefs(appStore);
-
-<Welcome v-show="welcoming" />
-```
-
-### 源码：
+# 开屏动画组件源码：
 
 ```vue
 
