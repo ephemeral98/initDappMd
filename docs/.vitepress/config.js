908ffa6454dbaace22df409d8ca2dfb7891c863c
ephemeral98 <3502969036@qq.com>
2023-06-01 16:58:24 +0800

    改为文件夹分类

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,76 +6,93 @@ export default defineConfig({
   themeConfig: {
     sidebar: [
       {
-        text: 'Guide',
+        text: '常用组件',
         items: [
-          { text: 'BpButton', link: '/BpButton/index' },
-          { text: '关闭图标(BpClose)', link: '/BpClose/index' },
+          { text: 'BpButton', link: '/BPComponents/BpButton/index' },
+          { text: '关闭图标(BpClose)', link: '/BPComponents/BpClose/index' },
           {
-            text: '表单组件(BpForm)', link: '/BpForm/index',
+            text: '表单组件(BpForm)',
+            link: '/BPComponents/BpForm/index',
             items: [
               {
-                text: '表单组件(源码)', link: '/BpForm/source'
-              }
-            ],
-            collapsed: true,
-          },
-          {
-            text: '选项卡组件(BpTab)', link: '/BpTab/index',
-            items: [
-              {
-                text: '横向选项卡组件(源码)', link: '/BpTab/source',
-              },
-              {
-                text: '纵向选项卡组件(源码)', link: '/BpTab/colSource',
+                text: '表单组件(源码)',
+                link: '/BPComponents/BpForm/source',
               },
             ],
             collapsed: true,
           },
           {
-            text: '全局转圈圈(GlobalLoading)', link: '/GlobalLoading/index',
-            items: [
-              { text: '全局转圈圈(源码)', link: '/GlobalLoading/source' },
-            ],
-            collapsed: true,
-          },
-          {
-            text: '指针光标组件(MousePoint)', link: '/MousePoint/index',
-            items: [
-              { text: '指针光标组件(源码)', link: '/MousePoint/source' },
-            ],
-            collapsed: true,
-          },
-          {
-            text: '开屏动画(Welcome)', link: '/Welcome/index',
-            items: [
-              { text: '开屏动画(源码)', link: '/Welcome/source', }
-            ],
-            collapsed: true,
-          },
-          {
-            text: 'Swiper(BpSwiper)', link: '/BpSwiper/index',
+            text: '选项卡组件(BpTab)',
+            link: '/BPComponents/BpTab/index',
             items: [
               {
-                text: 'BpSwiper(源码)', link: '/BpSwiper/source'
-              }
-            ],
-            collapsed: true,
-          },
-          {
-            text: '倒计时(Countdown)', link: '/Countdown/index',
-            items: [
-              {
-                text: '离开始与离结束(组件)(源码)', link: '/Countdown/source'
+                text: '横向选项卡组件(源码)',
+                link: '/BPComponents/BpTab/source',
               },
               {
-                text: 'useCountdown(源码)', link: '/Countdown/hookSource'
-              }
+                text: '纵向选项卡组件(源码)',
+                link: '/BPComponents/BpTab/colSource',
+              },
+            ],
+            collapsed: true,
+          },
+          {
+            text: '转圈圈(BpLoad)',
+            link: '/BPComponents/BpLoad/index',
+            items: [{ text: '转圈圈(源码)', link: '/BPComponents/BpLoad/source' }],
+            collapsed: true,
+          },
+          {
+            text: '指针光标组件(MousePoint)',
+            link: '/BPComponents/MousePoint/index',
+            items: [{ text: '指针光标组件(源码)', link: '/BPComponents/MousePoint/source' }],
+            collapsed: true,
+          },
+          {
+            text: '开屏动画(Welcome)',
+            link: '/BPComponents/Welcome/index',
+            items: [{ text: '开屏动画(源码)', link: '/BPComponents/Welcome/source' }],
+            collapsed: true,
+          },
+          {
+            text: 'Swiper(BpSwiper)',
+            link: '/BPComponents/BpSwiper/index',
+            items: [
+              {
+                text: 'BpSwiper(源码)',
+                link: '/BPComponents/BpSwiper/source',
+              },
+            ],
+            collapsed: true,
+          },
+          {
+            text: '倒计时(Countdown)',
+            link: '/BPComponents/Countdown/index',
+            items: [
+              {
+                text: '离开始与离结束(组件)(源码)',
+                link: '/BPComponents/Countdown/source',
+              },
+              {
+                text: 'useCountdown(源码)',
+                link: '/BPComponents/Countdown/hookSource',
+              },
             ],
             collapsed: true,
           },
         ],
         collapsible: true,
         collapsed: false,
+      },
+      {
+        text: '常用方法',
+        items: [
+          {
+            text: 'allowAccounts',
+            link: '/BPFunction/allowAccounts/index',
+            items: [{ text: 'allowAccounts源码', link: '/BPFunction/allowAccounts/source' }],
+          },
+        ],
       },
     ],
   },
